c4d0aec691faee38ba2323b646047b4fbb0cd2e2
bosh <ohwaitnvm@gmail.com>
2010-02-14 16:28:30 -0500

    First two green test cases, done BDD style, horray.

--- lib/simpLex.rb
+++ lib/simpLex.rb
@@ -1,2 +1,12 @@
 #But first, a short: Lolbuzz
 #(1..100).each{|i| puts((i%3==0) ? ((i%5==0)? "fizzbuzz" : "fizz" ) : ((i%5==0)? "buzz" : i) )}
+
+class Lexer
+  attr_accessor :buffer
+  def initialize()
+    load_buffer
+  end
+  def load_buffer
+    @buffer = "simple buffer"
+  end
+end
