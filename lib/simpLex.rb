a867b43402bf52039bb47de8b68d520463000cad
bosh <ohwaitnvm@gmail.com>
2010-02-25 20:02:53 -0500

    Formatting

--- lib/simpLex.rb
+++ lib/simpLex.rb
@@ -1,30 +1,27 @@
 class Lexer
   attr_accessor :buffer, :options
-  def initialize(file, options = {})
+  def initialize( file, options = {} )
     if File.exists?(file)
-      @options = options
-      @buffer = Buffer.new(File.open(file).read, @options[:dirty])
+      @options  = options
+      @buffer   = Buffer.new( File.open(file).read, @options[:dirty] )
       if options[:full] : full_analysis() end
     else
       puts "Cannot find the input file: #{file}\nType Use --help for more options."
       exit(0)
     end
   end
+  
+  def complete?;  @buffer.finished? end
+  def next_token; @buffer.get_next_token.tokenized end
+  
   def full_analysis
     prepare_outfile
     until complete?
       emit next_token
     end
   end
-  def complete?
-    @buffer.finished?
-  end
-  def next_token
-    @buffer.get_next_token.tokenized
-  end
-  def emit(str)
-    if @options[:stdout]
-      print str
+  def emit( str )
+    if @options[:stdout] : print str
     elsif @options[:target]
       File.open(@options[:target], 'a') {|f| f.write(str) }
     end
@@ -44,35 +41,26 @@ class Lexer
 end
 
 class Buffer
-  @@ids = []
   attr_accessor :contents, :current, :current_head, :style
   def initialize(str, dirty = false, style = :default)
     @contents = str.strip
-    @current = @current_head = 0
-    @style = style
+    @current  = @current_head = 0
+    @style    = style
     clean! unless dirty #Heh heh heh
   end
-  def clean!
-    @contents.gsub!(/\{.*?(\}|\z)/m, "") #Strip {} comments, including nonclosing terminal ones
-  end
-  def finished?
-    @current_head >= @contents.length #Meaning current is beyond referenceable characters
-  end
+  
+  def clean!;       @contents.gsub!(/\{.*?(\}|\z)/m, "") end #Strip {} comments
+  def finished?;    @current_head >= @contents.length end
+  def advance;      @current += 1             end
+  def back_up;      @current -= 1             end
+  def update_head;  @current_head = @current  end
+  
   def lookahead #Returns the character one ahead of the lookahead
     if @current+1 < @contents.size
       Token.new(@contents[@current+1,1]) #token so that it can call the _? methods
     else
       nil
     end
-  end
-  def advance
-    @current += 1
-  end
-  def back_up
-    @current -= 1
-  end
-  def update_head
-    @current_head = @current
   end
   def get_next_token
     if finished? : return nil end #Returns a nil if past end and still being called
@@ -122,6 +110,7 @@ class Buffer
     token
   end
 end
+
 class Token
   attr_accessor :text, :type, :value
   @@symbols = %w(+ * -  = < <= > >= <> . , : ; := .. ( ) [ ])
@@ -131,70 +120,45 @@ class Token
   @@relops = %w(= < <= >= > <>)
   @@ariths = %w(+ * -)
   @@groups = %w( ( ) [ ] )
+  
   def initialize(text, type = "", value = "")
     @text = text
     @type = type
     @value = value
     specify_type!
   end
-  def << (str)
-    @text << str
-  end
   def specify_type!
-    if keyword?
-      @type = "KEYWORD"
-    elsif string_literal?
-      @type = "STR"
-    elsif relational_op?
-      @type = "RELOP"
-    elsif arithmetic_op?
-      @type = "ARITHOP"
-    elsif grouping_symbol?
-      @type = "GROUP"
+    if keyword?               : @type = "KEYWORD"
+      elsif string_literal?   : @type = "STR"
+      elsif relational_op?    : @type = "RELOP"
+      elsif arithmetic_op?    : @type = "ARITHOP"
+      elsif grouping_symbol?  : @type = "GROUP"
     end
   end
-  def grouping_symbol?
-    @@groups.index(@text)
-  end
-  def relational_op?
-    @@relops.index(@text)
-  end
-  def arithmetic_op?
-    @@ariths.index(@text)
-  end
-  def string_literal?
-    @text.match( /\A\".*\"\z/m )
-  end
-  def identifier_head?
-    @text.match( /\A[a-zA-Z]\z/ )
-  end
-  def identifier_tail?
-    @text.match( /\A[a-zA-Z0-9_]*\z/ )
-  end
-  def digit?
-    @text.match( /\A[0-9]+\z/ )
-  end
-  def quote?
-    @text.match( /\A"\z/ )
-  end
-  def keyword?
-    @@keywords.index @text #return value. gives nil if not present
-  end
-  def symbol?
-    @@symbols.index @text #return value. gives nil if not present
-  end
-  def whitespace?
-    @text.match(/\A[\n\t\ ]*\z/) #return value. gives nil if not == ws
-  end
+  
+  def << (str);           @text << str  end
+  def grouping_symbol?;   @@groups.index(@text)   end
+  def relational_op?;     @@relops.index(@text)   end
+  def arithmetic_op?;     @@ariths.index(@text)   end
+  def keyword?;           @@keywords.index @text  end
+  def symbol?;            @@symbols.index @text   end
+  def string_literal?;    @text.match( /\A\".*\"\z/m )        end
+  def identifier_head?;   @text.match( /\A[a-zA-Z]\z/ )       end
+  def identifier_tail?;   @text.match( /\A[a-zA-Z0-9_]*\z/ )  end
+  def digit?;             @text.match( /\A[0-9]+\z/ )         end
+  def quote?;             @text.match( /\A"\z/ )              end
+  def whitespace?         @text.match( /\A[\n\t\ ]*\z/ )      end
+  
   def tokenized(style = :default)
-    if style == :default : "#{@type} #{@value}\n"
-      elsif style == :brackets : "[#{@type}: #{@value}]\n"
-      elsif style == :indent : "\t#{@type}: #{@value}\n"
-      elsif style == :angle : "<#{@type}, #{@value}>\n"
+    if      style == :default   : "#{@type} #{@value}\n"
+      elsif style == :brackets  : "[#{@type}: #{@value}]\n"
+      elsif style == :indent    : "\t#{@type}: #{@value}\n"
+      elsif style == :angle     : "<#{@type}, #{@value}>\n"
     end
   end
 end
-#Following should only run when not being used as a require from somewhere else
+
+#Following bit run only when not being used as a require from somewhere else
 instructions = "\nWelcome to simpLex,
 \ta simple Ruby lexical analyzer for a basic Pascal grammar.
 Usage:\n\t$ruby simpLex filename [opts]\nOptions:
@@ -202,8 +166,7 @@ Usage:\n\t$ruby simpLex filename [opts]\nOptions:
 \t-d - Dirty: Comments will not be automatically stripped from the file
 \t-a - All: Forces a full lexical analysis of the file
 \t-s - StdOut: Prints output to the command line (no save to a file)
-\t-o - Overwrite: Will overwrite the output file
-\t\tID, INT, SYMBOL, LITERAL\n
+\t-o - Overwrite: Will overwrite the output file\n
 \tNOTE: -a and -s both force a full run of the analyzer
 \t      -s has precedence in determining output type\n"
 if $0 == __FILE__
@@ -211,21 +174,19 @@ if $0 == __FILE__
     puts instructions
   else
     opts = {}
-    opts[:source] = "#{ARGV[0]}"
     ARGV[0].match(/\A(.*)\..*\z/)
     opts[:target] = "#{$1}_lex.txt"
+    opts[:source] = ARGV[0]
     args = ARGV.delete_at 0 #remove the program file from the arguments
     ARGV.each do |arg|
       case arg
-      when "-d" : opts[:dirty] = true
-      when "-a" : opts[:full] = true
-      when "-o" : opts[:overwrite] = true
-      when "-s"
-        opts[:stdout] = true
-        opts[:full] = true
-      when arg[0..1]=="-f" : #broken
-        opts[:overwrite] = true
-        opts[:target] = arg[2..-1]
+        when "-d" : opts[:dirty]      = true
+        when "-a" : opts[:full]       = true
+        when "-o" : opts[:overwrite]  = true
+        when "-s" : opts[:stdout]     = opts[:full] = true
+        when arg[0..1]=="-f" : #broken
+          opts[:overwrite] = true
+          opts[:target] = arg[2..-1]
       else
         puts "Unrecognized option: '#{arg}'. Attempting run anyway."
       end
