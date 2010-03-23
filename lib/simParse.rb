b61df53a3706b0e343a9d97482cd98dad460c00c
bosh <ohwaitnvm@gmail.com>
2010-03-22 20:04:18 -0400

    Added node class

--- lib/simParse.rb
+++ lib/simParse.rb
@@ -8,3 +8,19 @@ class Tree
     @root.tree_print
   end
 end
+class Node
+  attr_accessor :contents, :parent, :text
+  def initialize(parent, text)
+    @contents = []
+    @parent = parent
+    @text = text
+  end
+  def tree_print(level = 0)
+    level.times{print "\t"}
+    puts @text
+    @contents.each{|c| c.tree_print(level+1)}
+  end
+  def orphan!
+    @parent = nil
+  end
+end
